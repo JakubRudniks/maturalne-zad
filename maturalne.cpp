f88ddc9a96cf1b4cd78de61b2d55f68b794fed3f
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2020-11-26 10:52:43 +0100

    Add files via upload

---
+++ maturalne.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#incloude <fstream>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	 int liczba = i;
+	 
+	 cout<<"Podaj liczbe: "<<endl;
+	 
+    for (i=2; i<=liczba; i++)
+      while (liczba %i ==0)
+	{
+      	liczba/=i;
+      	
+      	if(i==y)
+	}
+      
+	{
+       liczba++;
+    }
+    
+    {
+       cout << i << ","; 
+       x= i;
+    }
+     
+     ifstream odczyt("liczby.txt");
+	 	odczyt>>i;
+	
+	odczyt>>i;
+	
+	cout<<"Suma wczytanych liczb wynosi: "<<i,x;
+	
+	cin.get();
+	
+	odczyt.close(); 
+     
+	return 0;
+}
+
+
+
